0f684859530e081672e45e7c1d79c37e0a862928
ed333a <3174941841@qq.com>
2026-05-24 21:37:48 +0800

    更新文章 id 自动生成逻辑，防止报错

--- src/utils/post-utils.ts
+++ src/utils/post-utils.ts
@@ -39,15 +39,15 @@ if (devConfig.debug) {
     console.log("post id(s): " + (postIDs.size > 0 ? '[' + Array.from(postIDs).join(', ') + ']' : "[]"))
 }
 
-// 开始处理
-for (const post of postFiles) {
+export function generateIdForPost(post: string): string {
+    console.log(`post: ${post}`);
     const content = fs.readFileSync(post, "utf-8");
-    if(content.includes("postID:")) continue; // 如果文章中已有 postID 则跳过处理
+    if(content.includes("postID:")) return "";  // 如果文章中已有 postID 直接返回空字符串
 
     const match = content.match(/^---\r?\n([\s\S]*?)\r?\n---/);
     if (!match) {
         console.log(`[Post-utils] ⚠ 跳过无 frontmatter 文件: ${post}`);
-        continue;
+        return ""; // 无 formatter 直接返回空字符串
     }
 
     // 生成 crc32 校验值
@@ -59,9 +59,10 @@ for (const post of postFiles) {
     } while (postIDs.has(final));
 
     postIDs.add(final);
-    const newFrontmatter = `---\n${match[1].trimEnd()}\npostID: ${final} # 不要修改这个项目的值\n---`
+    const newFrontmatter = `---\n${match[1].trimEnd()}\npostID: ${final} # 自动生成, 不要修改这个项目的值\n---`
     const newContent = content.replace(match[0], newFrontmatter);
     fs.writeFileSync(post, newContent);
 
     console.log(`[Post-utils] 为文章 ${post} 生成了文章 id: ${final}.`)
+    return final;
 }
