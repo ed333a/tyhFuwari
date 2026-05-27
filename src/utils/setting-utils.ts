137585b45b3a6ec06c65d6fbb0c568ed23007b9b
ed333 <ed333.email@qq.com>
2026-05-27 16:20:25 +0800

    评论适配暗色主题

--- src/utils/setting-utils.ts
+++ src/utils/setting-utils.ts
@@ -1,3 +1,5 @@
+import { commentCfg } from "@/types/config";
+
 export function getDefaultHue(): number {
 	const fallback = "250";
 	const configCarrier = document.getElementById("config-carrier");
@@ -19,17 +21,24 @@ export function setHue(hue: number): void {
 }
 
 export function applyThemeToDocument(theme: string) {
+	const artalkEle = document.getElementById("Comments");
 	switch (theme) {
 		case "light":
+			// artalk 日间模式
+			if (artalkEle) artalkEle.classList.remove("atk-dark-mode");
 			document.documentElement.classList.remove("dark");
 			break;
 		case "dark":
+			// artalk 夜间模式
+			if (artalkEle) artalkEle.classList.add("atk-dark-mode");
 			document.documentElement.classList.add("dark");
 			break;
 		case "auto":
 			if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+				if (artalkEle) artalkEle.classList.add("atk-dark-mode");
 				document.documentElement.classList.add("dark");
 			} else {
+				if (artalkEle) artalkEle.classList.remove("atk-dark-mode");
 				document.documentElement.classList.remove("dark");
 			}
 			break;
